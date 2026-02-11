24eea96d5577e31a45a624ff8469717c40df8140
Chester025 <toniyeste@paucasesnovescifp.cat>
2026-02-11 11:31:03 +0100

    He hecho el json

---
+++ Models/Bossesdiablo.js
@@ -0,0 +1,10 @@
+class Bossesdiablo {
+    constructor(nombre, titulo, tipo, vida_base, juego) {
+        this.nombre = nombre;
+        this.titulo = titulo;
+        this.tipo = tipo;
+        this.vida_base = vida_base;
+        this.juego = juego;
+    }
+}
+module.exports = Bossesdiablo;
---
+++ routes/Bossesdiablo-routes.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+
+const bossesdialoController = require("../Controller/Bossesdiablo-ctroller.js");
+
+module.exports = router;
